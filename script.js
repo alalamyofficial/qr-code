99d315bc6ea90f8efc3c5f0a04411b3c42105698
alalamyofficial <62678972+alalamyofficial@users.noreply.github.com>
2021-01-05 03:18:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let input = document.querySelector('input');
+let button = document.querySelector('button');
+
+
+let qrcode = new QRCode(document.querySelector('#qrcode'), {
+    // text: "First Code",
+    width: 250,
+    height: 250,
+    colorDark : "#000000",
+    colorLight : "#ffffff",
+    correctLevel : QRCode.CorrectLevel.H
+});
+
+button.addEventListener('click',() =>{
+
+    let inputValue = input.value
+    qrcode.makeCode(inputValue)
+
+});
+
+
+
